4ae123dc0b146adf9c6e438f82a4c9155467e0a1
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-11-18 14:26:23 +0100

    remove node_ref, use node_data directly

--- include/yaml-cpp/node/detail/node_data.h
+++ include/yaml-cpp/node/detail/node_data.h
@@ -25,6 +25,7 @@ class YAML_CPP_API node_data {
   node_data();
   node_data(const node_data&) = delete;
   node_data& operator=(const node_data&) = delete;
+  node_data& operator=(node_data&&) = default;
 
   void mark_defined();
   void set_mark(const Mark& mark);
