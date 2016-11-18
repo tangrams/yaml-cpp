a7be1c93aca85d1fd23ec22a8d7145d6f39ec603
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-11-18 14:26:23 +0100

    remove node_ref, use node_data directly

--- src/nodeevents.h
+++ src/nodeevents.h
@@ -34,7 +34,7 @@ class NodeEvents {
     anchor_t _CreateNewAnchor() { return ++m_curAnchor; }
 
    private:
-    typedef std::map<const detail::node_ref*, anchor_t> AnchorByIdentity;
+    typedef std::map<const detail::node_data*, anchor_t> AnchorByIdentity;
     AnchorByIdentity m_anchorByIdentity;
 
     anchor_t m_curAnchor;
@@ -49,7 +49,7 @@ class NodeEvents {
   detail::shared_memory_holder m_pMemory;
   detail::node* m_root;
 
-  typedef std::map<const detail::node_ref*, int> RefCount;
+  typedef std::map<const detail::node_data*, int> RefCount;
   RefCount m_refCount;
 };
 }
