d5621090014af9e835e51c74d4226990a5a67a78
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-12-10 17:53:50 +0100

    move mark_defined/add_dependency to node.cpp
    - avoid inline stuff that is rarely used

--- src/node.cpp
+++ src/node.cpp
@@ -1,4 +1,5 @@
 #include "yaml-cpp/node/node.h"
+#include "yaml-cpp/node/detail/node.h"
 #include "yaml-cpp/nodebuilder.h"
 #include "nodeevents.h"
 
@@ -8,5 +9,32 @@ Node Clone(const Node& node) {
   NodeBuilder builder;
   events.Emit(builder);
   return builder.Root();
+}
+
+namespace detail {
+void node::mark_defined() {
+  if (is_defined())
+    return;
+
+  m_pRef->mark_defined();
+
+  if (m_dependencies) {
+    for (auto& it : *m_dependencies) {
+      it->mark_defined();
+    }
+    m_dependencies.reset();
+  }
+}
+
+void node::add_dependency(node& rhs) {
+  if (is_defined())
+    rhs.mark_defined();
+  else {
+    if (!m_dependencies) {
+      m_dependencies = std::unique_ptr<nodes>(new nodes);
+    }
+    m_dependencies->insert(&rhs);
+  }
+}
 }
 }
